e4db470cafe007120fa8070339e22230e253feaa
Rizon Kumar <rizon.kumar.rahi@gmail.com>
2023-08-27 16:58:46 +0530

    Updated the CSS

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,19 +5,20 @@ const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
 
   const toggleSignInForm = () => {
-    setIsSignInForm(!isSignInForm); // toogle feature true/false
+    setIsSignInForm(!isSignInForm); // toggle feature true/false
   };
 
   return (
-    <div>
+    <div className="bg-black min-h-screen flex flex-col items-center justify-center relative">
       <Header />
-      <div className="absolute">
+      <div className="absolute inset-0">
         <img
+          className="w-full h-full object-cover"
           src="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg"
           alt="bg-img"
         />
       </div>
-      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+      <form className="w-[350px] p-12 bg-black text-white rounded-lg relative z-10">
         <h1 className="font-semibold text-3xl py-4">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
@@ -25,23 +26,23 @@ const Login = () => {
           <input
             type="text"
             placeholder="Full Name"
-            className="p-4 my-4 w-full bg-gray-700"
+            className="p-4 my-4 w-full bg-gray-700 placeholder-gray-400"
           />
         )}
         <input
           type="text"
           placeholder="Email Address"
-          className="p-4 my-4 w-full bg-gray-700"
+          className="p-4 my-4 w-full bg-gray-700 placeholder-gray-400"
         />
         <input
           type="password"
           placeholder="Password"
-          className="p-4 my-4 w-full bg-gray-700"
+          className="p-4 my-4 w-full bg-gray-700 placeholder-gray-400"
         />
-        <button className="p-4 my-6 bg-red-700 w-full rounded-lg ">
+        <button className="p-4 my-6 bg-red-700 w-full rounded-lg font-bold">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
-        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+        <p className="py-4 cursor-pointer font-semibold" onClick={toggleSignInForm}>
           {isSignInForm
             ? "New to Netflix? Sign up now."
             : "Already registered? Sign In Now."}
