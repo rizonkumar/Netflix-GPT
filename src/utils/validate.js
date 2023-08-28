a69250c82d27f650745080db292ccc097b95a0b3
Rizon Kumar <rizon.kumar.rahi@gmail.com>
2023-08-28 19:31:58 +0530

    Optimized Code

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,10 +1,17 @@
 export const checkValidData = (email, password) => {
-
-    const isValidEmail = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email);
-    const isValidPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
-
-    if(!isValidEmail) return "Email Id is not valid";
-    if(!isValidPassword) return "Password is not valid"
-
-    return null;    // no errors
-};
+    const validationRules = [
+      {
+        isValid: /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email),
+        errorMessage: "Email Id is not valid",
+      },
+      {
+        isValid: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password),
+        errorMessage: "Password is not valid",
+      },
+    ];
+  
+    const invalidRule = validationRules.find(rule => !rule.isValid);
+  
+    return invalidRule ? invalidRule.errorMessage : null;
+  };
+  
