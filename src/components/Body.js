1f3523c832486f6a2e30e67728677222a23aad60
Rizon Kumar <rizon.kumar.rahi@gmail.com>
2023-08-29 11:04:39 +0530

    Fixed Code

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,16 @@
 import Login from "./Login";
 import Browse from "./Browse";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useDispatch } from "react-redux";
+import { addUser, removeUser } from "../utils/userSlice";
 
 const Body = () => {
+  const dispatch = useDispatch();
+
+
   const appRouter = createBrowserRouter([
     {
       path: "/",
@@ -13,6 +21,17 @@ const Body = () => {
       element: <Browse />,
     },
   ]);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName } = user;
+        dispatch(addUser({ uid: uid, email: email, displayName: displayName }));
+      } else {
+        dispatch(removeUser());
+      }
+    });
+  }, []);
 
   return (
     <div>
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,21 +1,12 @@
-export const checkValidData = (name, email, password) => {
-    const validationRules = [
-      {
-        isValid: /^[a-zA-Z\s]+$/.test(name), // Only letters and spaces allowed
-        errorMessage: "Name is not valid",
-      },
-      {
-        isValid: /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email),
-        errorMessage: "Email Id is not valid",
-      },
-      {
-        isValid: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password),
-        errorMessage: "Password is not valid",
-      },
-    ];
+  export const checkValidData = (email, password) => {
+    const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(
+      email
+    );
+    const isPasswordValid =
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
   
-    const invalidRule = validationRules.find(rule => !rule.isValid);
+    if (!isEmailValid) return "Email ID is not valid";
+    if (!isPasswordValid) return "Password is not valid";
   
-    return invalidRule ? invalidRule.errorMessage : null;
+    return null;
   };
-  
