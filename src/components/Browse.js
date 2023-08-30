a25f27f78c1c22dc6e8befd7d2002805151fd603
Rizon Kumar <rizon.kumar.rahi@gmail.com>
2023-08-30 11:41:54 +0530

    Refactored the Code

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,11 +1,14 @@
-import Header from "./Header"
+import Header from "./Header";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 
 const Browse = () => {
+  useNowPlayingMovies();
+
   return (
     <div>
       <Header />
     </div>
-  )
-}
+  );
+};
 
-export default Browse
+export default Browse;
