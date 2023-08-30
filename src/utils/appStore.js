30c28059ffed61357a9b742aef6039e00bbe3941
Rizon Kumar <rizon.kumar.rahi@gmail.com>
2023-08-30 11:42:19 +0530

    Movie Reducer

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,12 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import moviesReducer from "./movieSlices";
 
-const appStore = configureStore(
-    {
-        reducer: {
-            user: userReducer,
-        },
-    }
-)
+const appStore = configureStore({
+  reducer: {
+    user: userReducer,
+    movies: moviesReducer,
+  },
+});
 
 export default appStore;
