0b4750be9d20cdb91ed9b5f959cf84655ecdbe50
Rizon Kumar <rizon.kumar.rahi@gmail.com>
2023-08-30 11:42:57 +0530

    TMDB API

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,3 +6,12 @@ export const DEFAULT_USER_AVATAR =
 
 export const APP_BACKGROUND =
   "https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YmJkMWUyYTU5ZDcyNGI1ZDA4ZmYwY2I5OWJjYzE4NCIsInN1YiI6IjY0ZWUyMmFlMTgwZGVhMDEzOWEzZDIwYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BCYQxFiG5qVTDt9QYx9WsMN6CoNTL9bacALhgKs853Q",
+  },
+};
